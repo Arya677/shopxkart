9d1af0f9ff86cdc895f4db32e41a45af82da5836
Arya677 <arpit.arya.568@Gmail.com>
2025-05-21 16:51:54 +0530

    fix review variables error in index view

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,11 +4,12 @@ from store.models import Product, ReviewRating
 def index(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
 
+    reviews = {}
     for product in products:
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+        reviews[product.id] = ReviewRating.objects.filter(product_id=product.id, status=True)
 
     context = {
         'products': products,
-        'reviews': reviews,
+        'reviews': reviews,  # use this in template
     }
-    return render(request,'index.html',context)
+    return render(request, 'index.html', context)
